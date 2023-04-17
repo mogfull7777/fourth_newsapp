cd83694e6e7ffa9381fb8e433b57b2d5ba2d29de
mogfull7777 <anjunyoung94@gmail.com>
2023-04-17 23:53:38 +0900

    recap

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useEffect, useState} from 'react';
+import axios from "axios";
+import {Button, Card, Col, Container, Row} from "react-bootstrap";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+
+  const [news, setNews] = useState([]);
+
+    const getNews = async ()=> {
+      try {
+          const add = "https://newsapi.org/v2/everything?q=apple&from=2023-04-16&to=2023-04-16&sortBy=popularity&apiKey=4d991139f4ef4bcc8dc6cff0c1b0a93d";
+
+          const res = await axios.get(add)
+          console.log("aaaaaaa", res.data.articles)
+          setNews(res.data.articles)
+
+      } catch (err) {
+          console.log(err);
+      }
+    };
+
+        useEffect(() => {
+            getNews()
+        }, [])
+
+    return (
+        <Container>
+            <Row>
+                {news && news.map(item => (
+                    <Col className={'mt-5'}>
+                        <Card style={{ width: '18rem' }}>
+                            <Card.Img variant="top" src={item.urlToImage} />
+                            <Card.Body>
+                                <Card.Title>{`${item.title.slice(0, 30)}...`}</Card.Title>
+                                <Card.Text>
+                                    {`${item.content.slice(0, 150)}...`}
+                                </Card.Text>
+                                <Card.Text>
+                                    {`발행일 : ${item.publishedAt}`}
+                                </Card.Text>
+                                <Button variant="primary" href={item.url}>Go News</Button>
+                            </Card.Body>
+                        </Card>
+                    </Col>
+                ))}
+            </Row>
+        </Container>
+    );
+};
 
 export default App;
